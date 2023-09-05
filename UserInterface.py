f3908d84f9bedfb226bc824b9cf058f737e11abb
Saintleader <143585568+Saintleader@users.noreply.github.com>
2023-09-05 15:08:38 -0500

    Update UserInterface.py

--- UserInterface.py
+++ UserInterface.py
@@ -10,9 +10,9 @@ def inicio():
 
 def ingreso_de_datos():
     limite_de_registros = (int(input("Ingresar el Número de Registros que se desea consultar: ")))
-    departamento = str(input("Ingresar el departamento:").upper())
-    municipio = str(input("Ingresar el municipio:").upper())
-    cultivo = str(input("Ingresar el cultivo (ingrese solo la primera letra mayuscula):"))
+    departamento = str(input("Ingresar el departamento: ").upper())
+    municipio = str(input("Ingresar el municipio: ").upper())
+    cultivo = str(input("Ingresar el cultivo: ").capitalize())
     return [departamento, municipio, cultivo, limite_de_registros]
 
 
